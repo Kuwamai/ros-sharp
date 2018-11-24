3ec28471116144bddf0dbbda32645ecd14f6b397
Kuwamai <mitsumex333@gmail.com>
2018-11-25 00:07:55 +0900

    Create Float64.cs
    Add a Float64

---
+++ Libraries/RosBridgeClient/Messages/Standard/Float64.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace RosSharp.RosBridgeClient.Messages.Standard
+{
+    public class Float64 : Message
+    {
+        [JsonIgnore]
+        public const string RosMessageName = "std_msgs/Float64";
+        public long data;
+
+        public Float64()
+        {
+            data = 0;
+        }
+    }
+}
